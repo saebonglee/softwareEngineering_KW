0c1a5ba80d613b1f2c232ea11f47248e01a7210f
ymj307 <dymj307@gmail.com>
2023-03-30 14:58:07 +0900

    this is user A changing code

--- test1.js
+++ test1.js
@@ -1,12 +1,13 @@
 function addUpto(n) {
-    let total=0;
-    for (let i = 0; i <= n; i++) {
-        total += i;
-    }
-    return total;
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  console.log("this is userA");
+  return total;
 }
 
-console.log(addUpto(10));
+console.log(addUpto(20));
 //add up to slower function
 //using loop, recursion
 
@@ -14,4 +15,4 @@ console.log(addUpto(10));
 var t1 = performance.now();
 addUpto(100000000);
 var t2 = performance.now();
-console.log(`time : ${(t2-t1)/ 1000} seconds`)
+console.log(`time : ${(t2 - t1) / 1000} seconds`);
