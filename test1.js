113432c0e69fcdb4c9905a163f970f260fe3851b
saebonglee <ldmplay37@gmail.com>
2023-03-30 12:27:29 +0900

    original test1.js

---
+++ test1.js
@@ -0,0 +1,17 @@
+function addUpto(n) {
+    let total=0;
+    for (let i = 0; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+console.log(addUpto(10));
+//add up to slower function
+//using loop, recursion
+
+//show executing time
+var t1 = performance.now();
+addUpto(100000000);
+var t2 = performance.now();
+console.log(`time : ${(t2-t1)/ 1000} seconds`)
